1a93c7ec864a97b3c2fdc2a5ca256ec9d2853f5c
Tim <t.seryakov@gmail.com>
2020-12-17 20:09:17 +0300

    init
    Took 2 minutes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="text-blue-800 text-2xl">Hello</h1>
+    </div>
+  )
+}
+
+export default App
